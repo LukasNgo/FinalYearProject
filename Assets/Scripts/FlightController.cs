4696012c462a9afbd079591159a2b60549ee70c5
LukasNgo <32539108+LukasNgo@users.noreply.github.com>
2019-02-14 20:46:38 +0000

    testing different flying methods
    adding force to move the rigidbody attached to the headset based on position of the hands
    also tried moving by changing the transform but the movement was awkward so going back to adding force to move.

---
+++ Assets/Scripts/FlightController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightController : MonoBehaviour {
+
+    public Rigidbody _rigidbody;
+    public OVRInput.Button flightButton;
+    private OVRGrabbable ovrGrababble;
+    private float speed = 5;
+
+    // Use this for initialization
+    private void Start () {
+        ovrGrababble = GetComponent<OVRGrabbable>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (ovrGrababble.isGrabbed && OVRInput.GetDown(flightButton, ovrGrababble.grabbedBy.GetController()))
+        {
+            _rigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+        }
+        if (ovrGrababble.isGrabbed && OVRInput.GetUp(flightButton, ovrGrababble.grabbedBy.GetController()))
+        {
+            _rigidbody.velocity = Vector3.zero;
+        }
+
+    }
+}
