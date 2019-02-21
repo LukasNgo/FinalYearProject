b3def9db105a49ee558a2d83286228d3d3052a57
LukasNgo <32539108+LukasNgo@users.noreply.github.com>
2019-02-21 14:06:20 +0000

    improved flying
    flight speed can be controlled more precisely + smoother braking velocity

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -8,22 +8,44 @@ public class FlightController : MonoBehaviour {
     public OVRInput.Button flightButton;
     private OVRGrabbable ovrGrababble;
     private float speed = 5;
+    private Vector3 refVelocity = Vector3.zero;
 
-    // Use this for initialization
     private void Start () {
         ovrGrababble = GetComponent<OVRGrabbable>();
     }
 
     private void FixedUpdate()
     {
-        if (ovrGrababble.isGrabbed && OVRInput.GetDown(flightButton, ovrGrababble.grabbedBy.GetController()))
+        //Primary Index Trigger button is recommended to use because it is more sensitive to touch 
+        //and can adjust the speed by how much the button is pressed. Any other button can be used
+        //as well but the player won't be able to control the speed
+        if (flightButton == OVRInput.Button.PrimaryIndexTrigger)
         {
-            _rigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+            if (ovrGrababble.isGrabbed && OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger, ovrGrababble.grabbedBy.GetController()))
+            {
+                float speedMultiplier = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, ovrGrababble.grabbedBy.GetController());
+                _rigidbody.AddForce(transform.forward * speed * speedMultiplier, ForceMode.Acceleration);
+            }
+            if (ovrGrababble.isGrabbed && !OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, ovrGrababble.grabbedBy.GetController()))
+            {
+                _rigidbody.velocity = _rigidbody.velocity * 0.99f;
+                _rigidbody.angularVelocity = _rigidbody.angularVelocity * 0.99f;
+
+            }
         }
-        if (ovrGrababble.isGrabbed && OVRInput.GetUp(flightButton, ovrGrababble.grabbedBy.GetController()))
+        else
         {
-            _rigidbody.velocity = Vector3.zero;
+            if (ovrGrababble.isGrabbed && OVRInput.Get(flightButton, ovrGrababble.grabbedBy.GetController()))
+            {
+                _rigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+            }
+            if (ovrGrababble.isGrabbed && OVRInput.GetUp(flightButton, ovrGrababble.grabbedBy.GetController()))
+            {
+                _rigidbody.velocity = _rigidbody.velocity * 0.99f;
+                _rigidbody.angularVelocity = _rigidbody.angularVelocity * 0.99f;
+            }
         }
+
 
     }
 }
